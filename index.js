988ecc09e594d0ac92d0e7124ba6c1af5a864e4c
shielddd <88677908+shielddd@users.noreply.github.com>
2021-08-09 10:04:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -18,26 +18,7 @@ client.on("message", (message) => {
   logSentMessage(client, message, log);
 });
 
-client.on('webhookUpdate', async (channel ) => {
-  const guild = channel.guild;        
-  const action = await guild.fetchAuditLogs({ type: "WEBHOOK_CREATE"     }).then(async (audit) => audit.entries.first());
-      if (action.executor.id === client.user.id) return;
-        const position = channel.position;
-        const webhook = await channel.fetchWebhooks();
-        const rateLimitPerUser = channel.rateLimitPerUser;
-        var newChannel = await channel.clone()
-       channel.delete();
-      var loggingEmbed = new Discord.MessageEmbed()
-  .setTitle('Fz & Irvv')
-  .addField(`ID : ${action.executor.tag}`, `**Utilisateur :** ${action.executor}`)
-  .setColor(0x36393E)
-  .setFooter("Attention, un webhook vient d'être crée.")
-  newChannel.setPosition(position);
-  newChannel.setRateLimitPerUser(rateLimitPerUser)
-  let logChannel = client.channels.cache.get("757278263633510410")
-if(!logChannel) return;
-logChannel.send(loggingEmbed)
-})
+
 
 
 client.login(config.BOT_TOKEN);
